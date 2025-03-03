c829fab97f08d505779ccaa6e1d4da2ffcc044ee
daxpatel20 <daxpatel163@gmail.com>
2025-03-03 18:16:44 +0530

    Added one file

---
+++ eligillty.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std ;
+
+int main (){
+    int marks;
+
+    if(marks >= 50){
+        cout << "Congratulations! ";
+    }else if(marks <= 28){
+        cout << " your next year!";
+    } 
+    
+    return 0 ;
+}
